12394179a424aa594ad1e5e1fa3dc804b66da8f6
camibedoya47 <camibeyoya47@gmail.com>
2021-05-22 16:57:16 -0500

    inicio variables en el js

---
+++ js.js
@@ -0,0 +1,10 @@
+var numero_dado1;
+var numero_dado2;
+var posiciones = [0,-160,-320,-481,-642,-803];
+var dado1,dado2,boton_tirar;
+var suma_dados;
+var turno  = 1;
+var PUNTOS;
+
+// Cuando la pagina carga, ejecuta la función init
+window.onload = init;
